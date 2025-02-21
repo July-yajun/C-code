95821050dd04460c6872c089bc65098647515fae
July-yajun <2749088828@qq.com>
2025-02-21 17:30:35 +0800

    学习ing

---
+++ test-2-20/test-2-20/test.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+
+//define定义标识常量
+//#define NUM 100
+//
+//int main()
+//{
+//	printf("%d\n", NUM);
+//	int n = NUM;
+//	printf("%d\n", n);
+//	int arr  [NUM] = { 0 };
+//
+//	return 0;
+//}
+
+
+//define定义宏（有参数）
+//#define ADD(x,y) ((x)+(y))  //ADD---宏的名字    (x,y)---宏的参数  参数无类型   ((x)+(y))---宏体
+////宏是完成替换的
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int c=ADD(a, b);//替换后括号内为((a)+(b)) 
+//	printf("%d\n", c);
+//
+//	return 0;
+//}
+
