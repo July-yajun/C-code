d763ae713d69cf0fb22e7fef7f5425df12ad702a
July-yajun <2749088828@qq.com>
2025-02-26 15:34:23 +0800

    练习

---
+++ test-2-25/test-2-25/test.c
@@ -0,0 +1,85 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+
+//找四个整数最大值
+//int main()
+//{
+//	//int arr[4] = { 0 };
+//	//int i = 0;
+//	//while (i < 4)
+//	//{
+//	//	scanf("%d", &arr[i]);
+//	//	i++;
+//	//}
+//	////假设第一个元素为最大值
+//	//int max = arr[0];
+//	// i=1;
+//	//while (i < 4)
+//	//{
+//	//	if (arr[i] > max)
+//	//	{
+//	//		max = arr[i];
+//	//	}
+//	//	i++;
+//	//}
+//	//printf("%d\n", max);
+//
+//	
+//	//int i = 1;
+//	//int n = 0;
+//	//int max = 0;
+//	////假设第一个元素为最大值
+// //   scanf("%d", &max);
+//
+//	//while (i < 4)
+//	//{
+//	//	scanf("%d", &n);
+//	//	if (n > max)
+//	//		max = n;
+//	//	i++;
+//	//}
+//
+//	//printf("%d\n", max);
+//
+//	return 0;
+//}
+
+
+
+
+//计算球的体积
+//int main()
+//{
+//	double r = 0.0;
+//	double v = 0.0;//float 略有差异
+//	scanf("%lf", &r);
+//	v = 4 / 3.0 * 3.1415926 * r * r * r;
+//	printf("%.3lf\n", v);
+//
+//	return 0;
+//}
+
+
+
+//bmi指数
+//int main()
+//{
+//	int weight = 0;
+//	int hige = 0;
+//	scanf("%d %d", &weight, &hige);
+//
+//	float bmi = weight/(hige / 100.0)/(hige / 100.0);
+//
+//	printf("%.2f\n", bmi);
+//
+//	return 0;
+//
+//}
+
+
+
+
+
+
+//for循环
